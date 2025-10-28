936ea17e90fb30bf94b646eda9d5b8028cc38bfb
Anuraag Agrawal <anuraaga@gmail.com>
2025-10-28 13:44:10 +0900

    Print # of requests

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"sync"
+	"sync/atomic"
 )
 
 func main() {
 	var grp sync.WaitGroup
+	var cnt atomic.Uint64
+	defer func() {
+		fmt.Printf("executed %d requests\n", cnt.Load())
+	}()
 	for range 40 {
 		grp.Go(func() {
 			for {
+				cnt.Add(1)
 				var prots http.Protocols
 				prots.SetUnencryptedHTTP2(true)
 				cl := &http.Client{
@@ -21,7 +28,8 @@ func main() {
 				req, _ := http.NewRequest("GET", "http://localhost:8000/controlled", nil)
 				res, err := cl.Do(req)
 				if err != nil {
-					panic(err)
+					fmt.Println(err)
+					return
 				}
 				res.Body.Close()
 				if res.StatusCode != 200 {
